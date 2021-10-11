40d16ff9cdc67e40968bedf14274ce5f244cb422
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2021-10-11 20:43:21 +0900

    내림차순 정렬
    for문을 거꾸로도 사용할수 있다는것 잊지말자

--- sortAlgorithm/SortInside.java
+++ sortAlgorithm/SortInside.java
@@ -4,9 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Arrays;
- 
+ //배열 내림차순
+// for문을 거꾸로 사용할수 있다는것도 잊지말자
 //배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
-public class SoftInside {
+public class SortInside {
 	public static void main(String[] args) throws IOException {
  
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
